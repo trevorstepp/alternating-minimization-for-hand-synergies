93f799bfe59ff1cfa7c7de003802a5bf0a6be760
Trevor Stepp <181792165+trevorstepp@users.noreply.github.com>
2026-05-29 13:19:36 -0400

    remove unused files

--- model/test.py
+++ model/test.py
@@ -1,3 +1,8 @@
+"""
+Test I was doing to see if starting alternating minimization with learned synergies from 
+another subject (rather than random initialization) leads to better results for this subject.
+"""
+
 from model.utils.read_files import load_natural_grasps
 from model.utils.read_files import verify_saved_synergies
 from model.utils.save_files import save_active_repeats
@@ -8,7 +13,7 @@ load_dotenv()
 
 if __name__ == '__main__':
 
-    subject = "subj2"
+    subject = "subj8"
     filename = "Natural_Test_Data.mat"
 
     synergies = verify_saved_synergies(subject="subj2", filename="subj2_synergies.mat")
@@ -17,11 +22,11 @@ if __name__ == '__main__':
     n, t_s, m = synergies.shape
     print(f"n: {n}, t_s: {t_s}, m: {m}")
     K_j = T - t_s + 1
-    lambda1 = 0.2  # combination between group lasso and lasso (0=group lasso, 1=lasso)
-    alpha = 0.000175  # overall regularization strength
+    lambda1 = 0.25  # combination between group lasso and lasso (0=group lasso, 1=lasso)
+    alpha = 0.000085  # overall regularization strength
     altMin = AlternatingMinModel(T, t_s, m, n, K_j, G, subject, V, seed=1, lambda1=lambda1, alpha=alpha)
     altMin.s_list = [synergies[:, :, j] for j in range(altMin.m)]
     print([altMin.s_list[j].shape for j in range(altMin.m)])
     altMin.S = altMin.build_S(altMin.s_list, altMin.m)
-    print(altMin.S.shape)
+    altMin.solve()
     save_active_repeats(altMin.S, subject, tol=1e-4)
